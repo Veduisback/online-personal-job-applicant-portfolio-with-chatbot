b668767112b7cbc528caa8c47c3e736b73af95ae
Vedang Jaiswal <veduisback@gmail.com>
2026-06-17 18:26:00 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyC70V_HMClWJGBAkNU05LXaicnRQBsfcls",
+  authDomain: "online-job-applicant-portfolio.firebaseapp.com",
+  projectId: "online-job-applicant-portfolio",
+  storageBucket: "online-job-applicant-portfolio.firebasestorage.app",
+  messagingSenderId: "775451627598",
+  appId: "1:775451627598:web:0c353e3e31f4197c10331f"
 };
 
 firebase.initializeApp(firebaseConfig);
